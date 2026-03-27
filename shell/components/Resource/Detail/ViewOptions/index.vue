598369f7619c4774c0666badbd53ccfac42cc384
Marcelo Fukumoto <marcelo.fukumoto@suse.com>
2026-03-27 13:23:47 +0100

    [16647] Fixed the TitleBar config to position the buttons properly
    Changed the TitleBar to only add margin to the first level
    Changed the name CONFIG -> DETAIL for the navigation to avoid confusion with Show Configuration
    Added full mount spapshot tests for ViewOptions and TitleBar

--- shell/components/Resource/Detail/ViewOptions/index.vue
+++ shell/components/Resource/Detail/ViewOptions/index.vue
@@ -1,7 +1,7 @@
 <script lang="ts">
 import { useRouter } from 'vue-router';
 import { computed, ref, watch } from 'vue';
-import { _CONFIG, _GRAPH } from '@shell/config/query-params';
+import { _DETAIL, _GRAPH } from '@shell/config/query-params';
 import ButtonGroup from '@shell/components/ButtonGroup';
 import { useCurrentView } from '@shell/components/Resource/Detail/ViewOptions/composable';
 </script>
@@ -14,8 +14,8 @@ const view = ref(currentView.value);
 const viewOptions = computed(() => {
   return [
     {
-      labelKey: 'resourceDetail.masthead.config',
-      value:    _CONFIG,
+      labelKey: 'resourceDetail.masthead.detail',
+      value:    _DETAIL,
     },
     {
       labelKey: 'resourceDetail.masthead.graph',

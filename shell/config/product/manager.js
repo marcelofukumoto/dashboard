7810604958567cbe5ea189c24867d81fc60a9724
Marcelo Fukumoto <marcelo.fukumoto@suse.com>
2026-07-13 18:20:27 +0200

    Sort list state on name if equal on state
    Replicates rancher/dashboard#18207 (QA-test corpus)

--- shell/config/product/manager.js
+++ shell/config/product/manager.js
@@ -19,7 +19,7 @@ import { DSL } from '@shell/store/type-map';
 import { BLANK_CLUSTER } from '@shell/store/store-types.js';
 import { markRaw } from 'vue';
 import {
-  STEVE_AGE_COL, STEVE_MGMT_CLUSTER_KUBE_VERSION, STEVE_MGMT_CLUSTER_PROVIDER, STEVE_NAMESPACE_COL, STEVE_STATE_COL
+  STEVE_AGE_COL, STEVE_MGMT_CLUSTER_KUBE_VERSION, STEVE_MGMT_CLUSTER_PROVIDER, STEVE_MGMT_STATE_COL, STEVE_NAMESPACE_COL
 } from '@shell/config/pagination-table-headers';
 
 export const NAME = 'manager';
@@ -193,7 +193,7 @@ export function init(store) {
     AGE,
     EXPLORER,
   ], [
-    STEVE_STATE_COL,
+    STEVE_MGMT_STATE_COL,
     {
       name:          'name',
       labelKey:      'tableHeaders.name',
